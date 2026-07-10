06ba0e4de1b624e85dc6d6d72d1aad3c1d4e7f41
juz41 <julek.zalewski@gmail.com>
2026-07-10 13:53:09 +0200

    added params

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,41 @@
 #include "scene.hpp"
 #include "scenereader.hpp"
 #include "printers.hpp"
+
+#include <cstring>
 #include <ctime>
 
 
+void print_usage() {
+    printf("Usage: \n");
+    printf("[resolution] [samples] [photons_per_light] [depth]\n");
+    printf("Defaults: resolution=256 samples=50 photons_per_light=10000 param4=3\n");
+}
 
 int main(int argc, char **argv) {
     srand(time(nullptr));
+
+    u32 resolution = 256;
+    u32 samples    = 50;
+    u32 photons_per_light     = 10000;
+    u32 depth     = 3;
+
+    if (argc > 1) {
+        if (std::strcmp(argv[1], "-h") == 0 || std::strcmp(argv[1], "--help") == 0) {
+            print_usage();
+            return 0;
+        }
+        resolution = std::atoi(argv[1]);
+    }
+    if (argc > 2) samples = std::atoi(argv[2]);
+    if (argc > 3) photons_per_light  = std::atoi(argv[3]);
+    if (argc > 4) depth  = std::atoi(argv[4]);
+
+    printf("Chosen paramters: \n");
+    printf("resolution: %i\n", resolution);
+    printf("samples: %i\n", samples);
+    printf("photons_per_light: %i\n", photons_per_light);
+    printf("depth: %i\n", depth);
 
     // auto scenes = ReadFile("./models/Box/glTF/Box.gltf");
     auto scenes = ReadFile("./models/Duck/glTF/Duck.gltf");
@@ -22,8 +51,7 @@ int main(int argc, char **argv) {
     scene.add_light(PointLight(vec3(-2.5f, -3.5f, 3.0f), green));
     scene.add_light(PointLight(vec3(-2.5f, 3.5f, 3.0f), red));
     print_camera(scene.get_camera());
-    scene.generate_image(256, 50, 10'000, 3);
+    scene.generate_image(resolution, samples, photons_per_light, depth);
 
     return 0;
 }
-
