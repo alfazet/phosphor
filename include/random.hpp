493ea3691d3f96be62c56be3797b06d5f417fb2a
Paweł Bielecki <contact@pbielecki.com>
2026-07-24 21:24:48 +0200

    make threads own RngState

--- include/random.hpp
+++ include/random.hpp
@@ -15,14 +15,18 @@ inline u32 pcg_random(RngState &rng) {
     return (((oldstate >> ((oldstate >> 28u) + 4u)) ^ oldstate) * 277803737u) >> 16u;
 }
 
-inline f32 random_float(RngState &rng) { return static_cast<f32>(pcg_random(rng)) / 65535.0f; }
-
-inline void pcg_seed(RngState &rng, u32 seed) {
+inline RngState pcg_seed(u32 seed) {
+    RngState rng;
     rng.state = 0u;
     rng.state = rng.state * 747796405u + 2891336453u;
     rng.state += seed;
     rng.state = rng.state * 747796405u + 2891336453u;
+    return rng;
 }
+
+inline RngState make_thread_rng(RngState base, u32 thread_index) { return pcg_seed(base.state + thread_index); }
+
+inline f32 random_float(RngState &rng) { return static_cast<f32>(pcg_random(rng)) / 65535.0f; }
 
 inline vec3 random_unit_vector(RngState &rng) {
     vec3 p;
