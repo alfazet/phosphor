4f88d5b05d782368128b2b19346517d48c3b1404
juz41 <julek.zalewski@gmail.com>
2026-09-23 20:28:15 +0200

    remove unnecessary padding

--- include/texture_meta.h
+++ include/texture_meta.h
@@ -5,14 +5,13 @@
 #include "typedefs.h"
 
 typedef struct GPU_ALIGN TextureMeta {
-    u32 atlas_offset; // offset into the atlas for mip level 0
+    u32 atlas_offset;
     u32 width;
     u32 height;
     u32 channels;
     // 4 * 4 = 16
 
     // total: 16
-    u8 _padding[16];
 } TextureMeta;
 
 #endif // PHOSPHOR_TEXTURE_META_H
